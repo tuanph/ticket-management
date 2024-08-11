b01a79e8f43077dde591eb2f21bb020619e8989e
TUANPH-HOME-PC <tuanpham@blogicsystems.com>
2024-08-11 21:55:52 +0700

    feat:  Setting up the Application core

---
+++ GloboTicket.TicketManagement/GloboTicket.TicketManagement.Application/Features/Events/Queries/GetEventDetail/CategoryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace GloboTicket.TicketManagement.Application.Features.Events.Queries.GetEventDetail
+{
+    public class CategoryDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
